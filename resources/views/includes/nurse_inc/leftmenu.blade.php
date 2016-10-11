bdd4c2f9c6807440cd37c16ac2e3154394803bdf
MbecheTheGeek <mbechesteve12@gmail.com>
2016-10-11 15:31:10 +0300

    NUrse Index

--- resources/views/includes/nurse_inc/leftmenu.blade.php
+++ resources/views/includes/nurse_inc/leftmenu.blade.php
@@ -6,9 +6,10 @@
                     <li>
                       <a href="#"><i class="glyphicon glyphicon-dashboard "></i> <span>Dashboard</span>
                       <a href="{{ URL::to('#')}}"><i class="glyphicon glyphicon-stats"></i> <span>Statistics</span>
-                      <a href="{{ URL::to('#')}}"><i class="glyphicon  glyphicon-user"></i> <span>Total Patients</span>       <span class="badge"><?php echo $data; ?></span>
-                      <a href="{{ URL::to('nurses.show')}}"><i class="glyphicon glyphicon-user"></i> <span>New Patients</span>    <span class="badge"><?php echo $data; ?></span>
-                     <a href="{{ URL::to('#')}}"><i class="glyphicon glyphicon-user"></i> <span>Waiting List</span>   <span class="badge">0</span>
+                      <a href="{{ URL::to('#')}}"><i class="fa fa-users"></i> <span>Total Patients</span>       <span class="badge"><?php echo $data; ?></span>
+                      <a href="{{ URL::to('nurses.show')}}"><i class="fa fa-users"></i> <span>New Patients</span>    <span class="badge"><?php echo $data; ?></span>
+                     <a href="{{ URL::to('#')}}"><i class="fa fa-users"></i> <span>Waiting List</span>   <span class="badge">0</span>
+                    <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
                     </li>
 
             </div>

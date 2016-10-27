a7de69a1d953feb4268f1541b3316e6beca6c5db
christopher <moketch.co@gmail.com>
2016-10-27 08:56:03 +0300

    all

--- resources/views/includes/doc_inc/head.blade.php
+++ resources/views/includes/doc_inc/head.blade.php
@@ -9,8 +9,7 @@
 	   <link href="{{ asset('css/bootstrap.min.css') }}" media="all" rel="stylesheet" type="text/css" />
    <link href="{{ asset('fontawesome/css/font-awesome.min.css') }}" media="all" rel="stylesheet" type="text/css" />
   <link href="{{ asset('css/metisMenu.css') }}" media="all" rel="stylesheet" type="text/css" />
-<link href="{{ asset('css/morris-0.4.3.min.css') }}" media="all" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('css/morris-0.4.3.min.css') }}" media="all" rel="stylesheet" type="text/css" />
  <link href="{{ asset('css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
-  <script type="text/javascript" src="{{ asset('js/modernizr.js') }}"></script>
 
-	  <script src="js/modernizr.js"></script>
+  <script type="text/javascript" src="{{ asset('js/modernizr.js') }}"></script>

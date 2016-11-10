440664f08faa9749083dcb54baf85a783d024aaa
MbecheTheGeek <mbechesteve12@gmail.com>
2016-11-10 13:39:43 +0300

    admin layout

--- resources/views/includes/admin_inc/leftmenu.blade.php
+++ resources/views/includes/admin_inc/leftmenu.blade.php
@@ -3,16 +3,17 @@
             <ul class="metismenu clearfix" id="menu">
  <li class="active"><a href="{{ url('/') }}"<i class="fa fa-home"></i>  <span>ADMIN-Dashboard</span></a></li>
 
- <li><a href="{{ url('/home') }}"><i class="fa fa-edit"></i> <span>HOME</span><span class="fa arrow"></span></a></li>
- <li><a href="{{ route('users.index') }}"><i class="fa fa-edit"></i> <span>USER</span><span class="fa arrow"></span></a></li>
- <li><a href="{{ route('roles.index') }}"><i class="fa fa-edit"></i> <span>ROLES</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ route('itemCRUD2.index') }}"><i class="fa fa-edit"></i> <span>ITEM</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ route('test.index') }}"><i class="fa fa-edit"></i> <span>TEST</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ url('/nurse') }}"><i class="fa fa-edit"></i> <span>NURSE</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ url('/doctor') }}"><i class="fa fa-edit"></i> <span>DOCTOR</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ url('/pharmacy') }}"><i class="fa fa-edit"></i> <span>PHARM</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ url('/manufacturer') }}"><i class="fa fa-edit"></i> <span>MANUFACTURER</span><span class="fa arrow"></span></a></li>
-<li><a href="{{ url('/patient') }}"><i class="fa fa-edit"></i> <span>PATIENT</span><span class="fa arrow"></span></a></li>
+ <li><a href="{{ url('/home') }}"><i class="fa fa-home"></i> <span>HOME</span></a></li>
+ <li><a href="{{ route('users.index') }}"><i class="fa fa-users"></i> <span>USER</span></a></li>
+ <li><a href="{{ route('roles.index') }}"><i class="fa fa-info"></i> <span>ROLES</span></a></li>
+<li><a href="{{ route('itemCRUD2.index') }}"><i class="fa fa-list"></i> <span>ITEM</span></a></li>
+<li><a href="{{ route('test.index') }}"><i class="fa fa-stethoscope"></i> <span>TEST</span></a></li>
+<li><a href="{{ url('/nurse') }}"><i class="fa fa-user-md"></i> <span>NURSE</span></a></li>
+<li><a href="{{ url('/doctor') }}"><i class="fa fa-user-md"></i> <span>DOCTOR</span></a></li>
+<li><a href="{{ url('/pharmacy') }}"><i class="fa fa-user-md"></i> <span>PHARM</span></a></li>
+<li><a href="{{ url('/manufacturer') }}"><i class="fa fa-industry"></i> <span>MANUFACTURER</span></a></li>
+<li><a href="{{ url('/patient') }}"><i class="fa fa-users"></i> <span>PATIENT</span></a></li>
+<li><a href="{{ url('/patient') }}"><i class="fa fa-cog"></i> <span>Master Configuration</span></a></li>
 
 
 

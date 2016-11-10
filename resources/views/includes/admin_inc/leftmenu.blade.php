3be98c2f7f8618d56c09407e230050a8a38317ac
MbecheTheGeek <mbechesteve12@gmail.com>
2016-11-10 13:50:43 +0300

    admin controller

--- resources/views/includes/admin_inc/leftmenu.blade.php
+++ resources/views/includes/admin_inc/leftmenu.blade.php
@@ -1,9 +1,9 @@
 <!--left menu start-->
           <div class="side-menu left" id="side-menu">
             <ul class="metismenu clearfix" id="menu">
- <li class="active"><a href="{{ url('/') }}"<i class="fa fa-home"></i>  <span>ADMIN-Dashboard</span></a></li>
+ 
 
- <li><a href="{{ url('/home') }}"><i class="fa fa-home"></i> <span>HOME</span></a></li>
+ <li><a href="{{ url('/admin') }}"><i class="fa fa-home"></i> <span>HOME</span></a></li>
  <li><a href="{{ route('users.index') }}"><i class="fa fa-users"></i> <span>USER</span></a></li>
  <li><a href="{{ route('roles.index') }}"><i class="fa fa-info"></i> <span>ROLES</span></a></li>
 <li><a href="{{ route('itemCRUD2.index') }}"><i class="fa fa-list"></i> <span>ITEM</span></a></li>

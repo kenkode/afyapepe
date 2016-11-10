57aea985ea6049b5a21d987974fac25ec46eb24f
MbecheTheGeek <mbechesteve12@gmail.com>
2016-11-10 16:27:21 +0300

    totalsales new layout

--- resources/views/pharmacy/totalsales.blade.php
+++ resources/views/pharmacy/totalsales.blade.php
@@ -17,19 +17,15 @@
                                                <table class="table table-striped">
                                                   <thead>
                                                       <tr>
-                                                          <th>No</th>
-                                                          <th>FirstName</th>
-                                                          <th>Surname</th>
-                                                          <th>Age</th>
-                                                          <th>Gender</th>
-                                                          <th>Drug</th>
-                                                          <th>Dosage</th>
-                                                          <th>Dosage Given</th>
-                                                          <th>Reasons</th>
-                                                          <th>Quantity</th>
-                                                          <th>Price</th>
-                                                          <th>Amount</th>
-                                                          <th>Date</th>
+                                                          <th><i class="fa fa-list fa-2x"></i> No</th>
+                                                          <th><i class="fa fa-user fa-2x"></i> Name</th>
+                                                           <th><i class="fa fa-medkit fa-2x"></i> Drug</th>
+                                                          <th><i class="fa fa-spoon fa-2x"></i> Dosage</th>
+                                                           <th><i class="fa fa-cart-plus fa-2x"></i> Quantity</th>
+                                                          <th><i class="fa fa-cc fa-2x"></i> Price</th>
+                                                          <th><i class="fa fa-shopping-cart fa-2x"></i> Amount</th>
+                                                          <th><i class="fa fa-calendar fa-2x"></i> Date</th>
+                                                          <th><i class="fa fa-gear fa-2x"></i> Action</th>
                                                     </tr>
                                                   </thead>
                                        <tbody>
@@ -37,20 +33,16 @@
                                       @foreach($patients as $patient)
                                            <tr>
                                                <td>{{$i}}</a></td>
-                                               <td>{{$patient->firstname}}</td>
-                                               <td>{{$patient->secondName}}</td>
-                                               <td>{{$patient->age}}</td>
-                                               <td><?php $gender=$patient->gender;?>
-                                                 @if($gender==1){{"Male"}}@else{{"Female"}}@endif
-                                               </td>
+                                               <td>{{$patient->firstname}} {{$patient->secondName}}</td>
+                                               
                                                <td>{{$patient->drugname}}</td>
                                                <td>{{$patient->dosage}}</td>
-                                               <td>{{$patient->dose_given}}</td>
-                                               <td>{{$patient->notes}}</td>
+                                               
                                                <td>{{$patient->quantity}}</td>
                                                <td>{{$patient->price}}</td>
                                                <td><?php $price=$patient->price; $quantity=$patient->quantity; echo $amount=$price*$quantity;?></td>
                                                <td>{{$patient->date}}</td>
+                                               <td><a class="btn btn-success" href="#"><i class="fa fa-file-pdf-o"></i></a> <a class="btn btn-info" href="#"><i class="fa fa-file"></i></a></td>
 
 
 

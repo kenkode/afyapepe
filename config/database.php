040cc0402c2f4f16029aa5c2421606e03e69715f
bwmurunga <bwmurunga@gmail.com>
2017-02-20 12:41:25 +0300

    Ussd content added

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'afya'),
+            'database' => env('DB_DATABASE', 'afya_new'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',

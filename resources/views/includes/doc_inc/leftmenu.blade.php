4e449775753885f9fe3f9479fc26c9016652e36f
MbecheTheGeek <mbechesteve12@gmail.com>
2017-02-20 12:25:56 +0300

    doctors new left menu

--- resources/views/includes/doc_inc/leftmenu.blade.php
+++ resources/views/includes/doc_inc/leftmenu.blade.php
@@ -24,11 +24,15 @@
 
                          <li class="active"><a href="{{ url('/') }}"<i class="fa fa-th-large"></i><span>DOCTOR-Dashboard</span></a></li>
 
-
+                          <li><a href="{{ url('allpatients') }}"><i class="fa fa-users"></i><span>All Patients</span><span class="fa arrow"></span></a></li>
                         <li><a href="{{ url('doctor') }}"><i class="fa fa-edit"></i> <span>Today's Patient</span><span class="fa arrow"></span></a></li>
                         <li><a href="{{ url('newpatients') }}"><i class="fa fa-pie-chart"></i><span>Waiting Patient</span><span class="fa arrow"></span></a></li>
-
-                        <li><a href="{{ url('allpatients') }}"><i class="fa fa-users"></i><span>All Patients</span><span class="fa arrow"></span></a></li>
+                          <li><a href="{{ url('allpatients') }}"><i class="fa fa-hospital-o"></i><span>Admitted Patients</span><span class="fa arrow"></span></a></li>
+                        <li><a href="{{ url('allpatients') }}"><i class="fa fa-stethoscope"></i><span>Test Results</span><span class="fa arrow"></span></a></li>
+                          <li><a href="{{ url('allpatients') }}"><i class="fa fa-money"></i><span>Your Fees</span><span class="fa arrow"></span></a></li>
+                          <li><a href="{{ URL::to('waitingList')}}">  <i class="glyphicon glyphicon-stats "></i> <span>Statictics</span><span class="fa arrow"></span></a></li>
+                          <li><a href="{{ URL::to('waitingList')}}">  <i class="glyphicon glyphicon-calendar "></i> <span>Calendar</span><span class="fa arrow"></span></a></li>
+                        <li> <a href="{{ URL::to('waitingList')}}">  <i class="glyphicon glyphicon-inbox "></i> <span>Email</span><span class="fa arrow"></span></a></li>
                         <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout<span class="fa arrow"></span></a></li>
 
 

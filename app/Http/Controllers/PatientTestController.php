bda901aa41459c1cd587c54b88ba57ec307ef157
christopher <moketch.co@gmail.com>
2017-03-31 16:58:53 +0300

    frii

--- app/Http/Controllers/PatientTestController.php
+++ app/Http/Controllers/PatientTestController.php
@@ -35,41 +35,89 @@ class PatientTestController extends Controller
 public function store(Request $request)
 {
 
-            $this->validate($request, [
-
+    $this->validate($request, [
           'doc_id' => 'required',
           'conditional' => 'required',
           'appointment_id' => 'required',
+          'biotests' => 'required',
+          'coagtests' => 'required',
+          'haemtests' => 'required',
+          'inftests' => 'required',
+          'autotests' => 'required',
+          'microtests' => 'required',
           ]);
 
-     $appointment=$request->get('appointment_id');
-     $pttids= Patienttest::where('appointment_id',$appointment)
-      ->first();
+ $appointment=$request->get('appointment_id');
+ $pttids= Patienttest::where('appointment_id',$appointment)
+  ->first();
 
      if (is_null($pttids)) {
      //  - add new
-$PatientTest = Patienttest ::create([
+   $PatientTest = Patienttest ::create([
   'doc_id' => $request->get('doc_id'),
   'appointment_id' => $request->get('appointment_id'),
               ]);
-          $ptid = $PatientTest->id;
+    $ptid = $PatientTest->id;
      } else {
-     // Already favorited - delete the existing
+     // Already test exist - just get the id
       $ptid =$pttids->id;
      }
+// Inserting Biochemistry tests
+     $Rtests=$request->biotests;
+     foreach($Rtests as $key) {
+   $patienttd = DB::table('patient_test_details')->insert([
+                'conditional_diagnosis' => $request->get('conditional'),
+                'patient_test_id' => $ptid,
+                'tests_reccommended' => $key,
+             ]);
+            }
 
-
-
-   $patienttd = DB::table('patient_test_details')->insertGetId(
-             [
-               'conditional_diagnosis' => $request->get('conditional'),
-               'patient_test_id' => $ptid,
-               'tests_reccommended' => $request->get('test'),
-
-
-             ]
-           );
-      return redirect()->route('showPatient', ['id' => $appointment]);
+            // Inserting Coagulation tests
+                 $Ctests=$request->coagtests;
+                 foreach($Ctests as $key) {
+               $patienttd = DB::table('patient_test_details')->insert([
+                            'conditional_diagnosis' => $request->get('conditional'),
+                            'patient_test_id' => $ptid,
+                            'tests_reccommended' => $key,
+                         ]);
+                        }
+                // Inserting Haematology tests
+                     $Htests=$request->haemtests;
+                     foreach($Htests as $key) {
+                   $patienttd = DB::table('patient_test_details')->insert([
+                                'conditional_diagnosis' => $request->get('conditional'),
+                                'patient_test_id' => $ptid,
+                                'tests_reccommended' => $key,
+                             ]);
+                            }
+                  // Inserting Immunology_Infective tests
+                       $Intests=$request->inftests;
+                       foreach($Intests as $key) {
+                     $patienttd = DB::table('patient_test_details')->insert([
+                                  'conditional_diagnosis' => $request->get('conditional'),
+                                  'patient_test_id' => $ptid,
+                                  'tests_reccommended' => $key,
+                               ]);
+                              }
+                // Inserting Immunology-Auto-Immune tests
+                     $Atests=$request->autotests;
+                     foreach($Atests as $key) {
+                   $patienttd = DB::table('patient_test_details')->insert([
+                                'conditional_diagnosis' => $request->get('conditional'),
+                                'patient_test_id' => $ptid,
+                                'tests_reccommended' => $key,
+                             ]);
+                            }
+                // Inserting Microbiology tests
+                     $Mtests=$request->microtests;
+                     foreach($Mtests as $key) {
+                   $patienttd = DB::table('patient_test_details')->insert([
+                                'conditional_diagnosis' => $request->get('conditional'),
+                                'patient_test_id' => $ptid,
+                                'tests_reccommended' => $key,
+                             ]);
+                            }
+  return redirect()->route('showPatient', ['id' => $appointment]);
       }
 
 
--- resources/views/doctor/show.blade.php
+++ resources/views/doctor/show.blade.php
@@ -237,7 +237,7 @@ return redirect('doctor.create');
 
                          <div class="form-group">
                              <label for="tag_list" class="col-md-4">Select Biochestry Test:</label>
-                                  <select class="test-multiple" multiple="multiple" style="width: 100%">
+                                  <select class="test-multiple" name="biotests[]" multiple="multiple" style="width: 100%">
                                     <?php $biotests=DB::table('lab_test')->where('category', '=','Biochemistry')->distinct()->get(['id','name']); ?>
                                     @foreach($biotests as $biotest)
                                            <option value='{{$biotest->id}}'>{{$biotest->name}}</option>
@@ -249,7 +249,7 @@ return redirect('doctor.create');
                       <div class="Coagulation box">
                         <div class="form-group">
                             <label for="tag_list" class="col-md-4">Select Coagulation  Test:</label>
-                                 <select class="test-multiple" multiple="multiple" style="width: 100%">
+                                 <select class="test-multiple" name="coagtests[]" multiple="multiple" style="width: 100%">
                                    <?php $coagtests=DB::table('lab_test')->where('category', '=','Coagulation')->distinct()->get(['id','name']); ?>
                                    @foreach($coagtests as $coagtest)
                                           <option value='{{$coagtest->id}}'>{{$coagtest->name}}</option>
@@ -260,7 +260,7 @@ return redirect('doctor.create');
                       <div class="Haematology box">
                         <div class="form-group">
                             <label for="tag_list" class="col-md-4">Select Haematology Test:</label>
-                                 <select class="test-multiple" multiple="multiple" style="width: 100%">
+                                 <select class="test-multiple" name="haemtests[]" multiple="multiple" style="width: 100%">
                                    <?php $haemtests=DB::table('lab_test')->where('category', '=','Haematology')->distinct()->get(['id','name']); ?>
                                    @foreach($haemtests as $haemtest)
                                           <option value='{{$haemtest->id}}'>{{$haemtest->name}}</option>
@@ -271,7 +271,7 @@ return redirect('doctor.create');
                       <div class="Immuno_infective box">
                         <div class="form-group">
                             <label for="tag_list" class="col-md-4">Select Immunology Infective Test:</label>
-                                 <select class="test-multiple" multiple="multiple" style="width: 100%">
+                                 <select class="test-multiple" name="inftests[]" multiple="multiple" style="width: 100%">
                                    <?php $imitests=DB::table('lab_test')->where('category', '=','Immunology_Infective')->distinct()->get(['id','name']); ?>
                                    @foreach($imitests as $imitest)
                                           <option value='{{$imitest->id}}'>{{$imitest->name}}</option>
@@ -282,7 +282,7 @@ return redirect('doctor.create');
                       <div class="Immuno_auto box">
                         <div class="form-group">
                             <label for="tag_list" class="col-md-4">Select Immunology Auto Immune Test:</label>
-                                 <select class="test-multiple" multiple="multiple" style="width: 100%">
+                                 <select class="test-multiple" name="autotests[]" multiple="multiple" style="width: 100%">
                                    <?php $imatests=DB::table('lab_test')->where('category', '=','Immunology-Auto-Immune')->distinct()->get(['id','name']); ?>
                                    @foreach($imatests as $imatest)
                                           <option value='{{$imatest->id}}'>{{$imatest->name}}</option>
@@ -293,8 +293,8 @@ return redirect('doctor.create');
                       <div class="Microbilogy box">
 
                         <div class="form-group">
-                            <label for="tag_list" class="col-md-4">Select Microbiology Test:</label>
-                                 <select class="test-multiple" multiple="multiple" style="width: 100%">
+                            <label for="tag_list" class="col-md-4">Select Microbiologye  Test:</label>
+                                 <select class="test-multiple" name="microtests[]" multiple="multiple" style="width: 100%">
                                    <?php $micrtests=DB::table('lab_test')->where('category', '=','Microbiology')->distinct()->get(['id','name']); ?>
                                    @foreach($micrtests as $micrtest)
                                           <option value='{{$micrtest->id}}'>{{$micrtest->name}}</option>

b8b382fae8cd896eb1325747d382e26b0ec93865
christopher <moketch.co@gmail.com>
2017-05-29 18:05:18 +0300

    stock

--- resources/views/manufacturer/manustock.blade.php
+++ resources/views/manufacturer/manustock.blade.php
@@ -2,18 +2,20 @@
 @section('title', 'Manufacturer')
 @section('content')
   <div class="content-page  equal-height">
-      
+    <?php
+    $id=Auth::id();
+    $manufacturer=DB::table('manufacturers')->where('user_id', Auth::id())->first();
+    $Mname = $manufacturer->name;
+    $Mid = $manufacturer->id;
+    ?>
       <div class="content">
           <div class="container">
-
-
-
-            <div class="wrapper wrapper-content animated fadeInRight">
+             <div class="wrapper wrapper-content animated fadeInRight">
                       <div class="row">
                           <div class="col-lg-11">
                           <div class="ibox float-e-margins">
                               <div class="ibox-title">
-
+                                    PHARMACY STOCKS
                                   <div class="ibox-tools">
 
                                       <a class="collapse-link">
@@ -39,26 +41,58 @@
                                   <div class="table-responsive">
                               <table class="table table-striped table-bordered table-hover dataTables-example" >
                               <thead>
-                                                                           
-        
+
+
                                                       <tr>
                                                           <th>No</th>
-                                                     <th>Drug Name</th>
-                                                          <th> Dosage</th>
+                                                          <th>Pharmacy</th>
+                                                          <th>County</th>
+                                                          <th>Drug Name</th>
                                                           <th>Dosage form</th>
                                                           <th>Quantity</th>
                                                           <th>Rate of Sales(Per Week)</th>
                                                           <th>Run of Rate</th>
-                                                          
+
                                                          </tr>
                                                     </tr>
                                                   </thead>
+                    <?php  $invents = DB::table('inventory')
+                         ->join('pharmacy','inventory.outlet_id','=','pharmacy.id')
+                         ->join('druglists','inventory.drug_id','=','druglists.id')
+                         ->select('pharmacy.name','pharmacy.county','inventory.created_at','inventory.quantity','inventory.strength',
+                         'inventory.strength_unit','druglists.drugname','druglists.DosageForm'
+                        )
+                         ->where([ ['druglists.Manufacturer','like','%'.$Mname.'%'],  ])
+                         ->whereIn('inventory.created_at', function($query)
+                             {
+                                 $query->select(DB::raw('max(inventory.created_at)'))
+                                       ->from('inventory')
+                                       ->join('pharmacy','inventory.outlet_id','=','pharmacy.id')
+                                       ->join('druglists','inventory.drug_id','=','druglists.id')
+                                       ->groupBy('pharmacy.name','druglists.drugname');
 
-                                                  <tbody>
-                                                    
+                             })
+                          ->get();
+                             $i=1;
+                          ?>
+                        <tbody>
+                          @foreach($invents as $stock)
+                          <tr>
+                          <td>{{$i}}</td>
+                          <td>{{$stock->name}}</td>
+                          <td>{{$stock->county}}</td>
+                          <td>{{$stock->drugname}}</td>
+                          <td>{{$stock->DosageForm}}</td>
+                          <td>{{$stock->quantity}}</td>
+                          <td>{{$stock->quantity}}</td>
+                          <td>{{$stock->quantity}}</td>
+
+                          </tr>
+                      <?php $i++; ?>
+                          @endforeach
 
                                                    </tbody>
-                                                 
+
                                                  </table>
                                                      </div>
 

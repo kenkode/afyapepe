22ee571f6880148a4d6cc714be7c828269ed3bbf
mbechesteve <mbechesteve12@gmail.com>
2017-07-21 10:53:59 +0300

    blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -65,6 +65,7 @@
     <script src="{{ asset('js/demo/peity-demo.js') }}" type="text/javascript"></script>
 
 
+
     <!-- jQuery UI -->
     <script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}" type="text/javascript"></script>
     <!-- GITTER -->
@@ -134,6 +135,79 @@
                 }
             });
         </script>
+         <script>
+        $(document).ready(function() {
+
+            var sparklineCharts = function(){
+                 $("#sparkline1").sparkline([34, 43, 43, 35, 44, 32, 44, 52], {
+                     type: 'line',
+                     width: '100%',
+                     height: '60',
+                     lineColor: '#1ab394',
+                     fillColor: "#ffffff"
+                 });
+
+                 $("#sparkline2").sparkline([24, 43, 43, 55, 44, 62, 44, 72], {
+                     type: 'line',
+                     width: '100%',
+                     height: '60',
+                     lineColor: '#1ab394',
+                     fillColor: "#ffffff"
+                 });
+
+                 $("#sparkline3").sparkline([74, 43, 23, 55, 54, 32, 24, 12], {
+                     type: 'line',
+                     width: '100%',
+                     height: '60',
+                     lineColor: '#ed5565',
+                     fillColor: "#ffffff"
+                 });
+
+                 $("#sparkline4").sparkline([24, 43, 33, 55, 64, 72, 44, 22], {
+                     type: 'line',
+                     width: '100%',
+                     height: '60',
+                     lineColor: '#ed5565',
+                     fillColor: "#ffffff"
+                 });
+
+                 $("#sparkline5").sparkline([1, 4], {
+                     type: 'pie',
+                     height: '140',
+                     sliceColors: ['#1ab394', '#F5F5F5']
+                 });
+
+                 $("#sparkline6").sparkline([5, 3], {
+                     type: 'pie',
+                     height: '140',
+                     sliceColors: ['#1ab394', '#F5F5F5']
+                 });
+
+                 $("#sparkline7").sparkline([2, 2], {
+                     type: 'pie',
+                     height: '140',
+                     sliceColors: ['#ed5565', '#F5F5F5']
+                 });
+
+                 $("#sparkline8").sparkline([2, 3], {
+                     type: 'pie',
+                     height: '140',
+                     sliceColors: ['#ed5565', '#F5F5F5']
+                 });
+            };
+
+            var sparkResize;
+
+            $(window).resize(function(e) {
+                clearTimeout(sparkResize);
+                sparkResize = setTimeout(sparklineCharts, 500);
+            });
+
+            sparklineCharts();
+
+
+        });
+    </script>
   <script>
   $(document).ready(function () {
   $('.group').hide();
